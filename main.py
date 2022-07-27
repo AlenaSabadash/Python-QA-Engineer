ffebc7ad12908be946a65c21e0741dc7d78a0db2
Alena Sabadash <alena_sbsh@mail.ru>
2022-07-27 19:43:32 +0500

    Новые правки по домашнему заданию №3

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import csv
 import json
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
+
+
+@dataclass
+class BookDto:
+    title: str
+    author: str
+    pages: int
+    genre: str
 
 
 @dataclass
@@ -9,29 +17,42 @@ class UserDto:
     gender: str
     address: str
     age: int
-    books: list[dict]
+    books: list[BookDto] = field(default_factory=list)
 
 
-def get_books():
+def get_books() -> list[BookDto]:
     with open("./assets/books.csv", "r") as f:
-        return list(csv.DictReader(f))
+        books = list(csv.DictReader(f))
+        return [
+            BookDto(
+                title=book["Title"],
+                author=book["Author"],
+                pages=int(book["Pages"]),
+                genre=book["Genre"],
+            )
+            for book in books
+        ]
 
 
-def get_users():
+def get_users() -> list[UserDto]:
     with open("./assets/users.json", "r") as f:
-        return json.load(f)
+        users = json.load(f)
+        return [
+            UserDto(
+                name=user["name"],
+                gender=user["gender"],
+                address=user["address"],
+                age=user["age"],
+            )
+            for user in users
+        ]
 
 
-def save_result_file(chunks, users):
+def save_result_file(books_chunks: list[list[BookDto]], users: list[UserDto]) -> None:
     users_result = []
-    for i, books in enumerate(chunks):
-        user = UserDto(
-            name=users[i]["name"],
-            gender=users[i]["gender"],
-            address=users[i]["address"],
-            age=users[i]["age"],
-            books=books,
-        )
+    for i, books in enumerate(books_chunks):
+        user = users[i]
+        user.books = books
 
         users_result.append(asdict(user))
 
@@ -39,15 +60,15 @@ def save_result_file(chunks, users):
         f.write(json.dumps(users_result))
 
 
-def chunkify(lst, n):
-    return [lst[i :: len(n)] for i in range(len(n))]
+def chunkify(books: list[BookDto], users: list[UserDto]) -> list[list[BookDto]]:
+    return [books[i :: len(users)] for i in range(len(users))]
 
 
 def main():
     books = get_books()
     users = get_users()
-    chunks = chunkify(books, users)
-    save_result_file(chunks, users)
+    books_chunks = chunkify(books, users)
+    save_result_file(books_chunks, users)
 
 
 if __name__ == "__main__":
